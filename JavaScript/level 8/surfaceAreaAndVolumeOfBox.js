6bc8c7c23aa48b5a8481c68d63a626a6bfc60f0c
PeterEremeev <65536236+PeterEremeev@users.noreply.github.com>
2023-09-28 15:21:36 +0100

    Create surfaceAreaAndVolumeOfBox.js

---
+++ JavaScript/level 8/surfaceAreaAndVolumeOfBox.js
@@ -0,0 +1,4 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+function getSize(width, height, depth) {
+  return [2 * width * height + 2 * height * depth + 2 * depth * width, width * height * depth];
+}
